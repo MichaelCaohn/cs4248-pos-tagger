256dc96cafec2572b0e5aef42bfa362ebae4a2ba
Liang Jun <au.liangjun@gmail.com>
2019-10-05 13:07:29 +0800

    Add naive Viterbi algorithm

--- buildtagger.py
+++ buildtagger.py
@@ -5,10 +5,51 @@ import math
 import sys
 import datetime
 import numpy as np
+from collections import defaultdict
+import json
 
+START = '<s>'
+END = '<\s>'
 
 def train_model(train_file, model_file):
-    # write your code here. You can add functions as well.
+    reader = open(train_file)
+    out_lines = reader.readlines()
+    reader.close()
+
+    tag_counts = defaultdict(lambda:0)
+    tag_transitions = defaultdict(lambda:defaultdict(lambda:0))
+    word_emissions = defaultdict(lambda:defaultdict(lambda:0))
+
+    for i in range(0, len(out_lines)):
+        cur_out_line = out_lines[i].strip()
+        cur_out_tokens = cur_out_line.split(' ')
+        # total_count += len(cur_out_tokens)
+
+        prev_tag = START
+        tag_counts[START] += 1
+
+        for j in range(0, len(cur_out_tokens)):
+            word, _,  tag = cur_out_tokens[j].rpartition('/')
+            # word = word.lower()
+            
+            tag_counts[tag] += 1
+            tag_transitions[prev_tag][tag] += 1
+            word_emissions[tag][word] += 1
+            prev_tag = tag
+
+        tag = END
+        tag_counts[END] += 1
+        tag_transitions[prev_tag][tag] += 1
+    
+    
+    file = open(model_file, 'w')
+    file.write(json.dumps({
+        'tag_counts':tag_counts, 
+        'tag_transitions':tag_transitions,
+        'word_emissions':word_emissions
+    }, indent=4, sort_keys=True))
+    file.close()
+
     print('Finished...')
 
 if __name__ == "__main__":
--- runtagger.py
+++ runtagger.py
@@ -5,10 +5,97 @@ import math
 import sys
 import datetime
 import numpy as np
+import json
+from collections import OrderedDict
+from collections import defaultdict
+
+START = '<s>'
+END = '<\s>'
+
+# TODO: Make the number of tags constant
+
+def viterbi(words, tag_counts, tag_transitions, start_transition, word_emissions):
+    tags = list(tag_transitions.keys())
+
+    matrix = [[None] * len(words) for i in range(len(tags))]
+    backpointer = [[None] * len(words) for i in range(len(tags))]
+    
+
+    word = words[0]
+    for s in range(len(tags)):
+        tag = tags[s]
+        if tag == START or tag == END: raise Exception
+        matrix[s][0] = start_transition[tag]/tag_counts[START] * word_emissions[tag][word]
+        backpointer[s][0] = 0
+
+    for t in range(1, len(words)):
+        word = words[t]
+        for s in range(len(tags)):
+            tag = tags[s]
+            emission = word_emissions[tag][word]/tag_counts[tag]
+            max = -1
+            argmax = None
+            for s2 in range(len(tags)):
+                tag2 = tags[s2]
+                p = matrix[s2][t-1] * tag_transitions[tag2][tag]/tag_counts[tag2] * emission
+                if p > max: 
+                    max = p
+                    argmax = s2
+            matrix[s][t] = max
+            backpointer[s][t] = argmax
+    
+
+    t = len(words)-1
+    max = -1
+    argmax = None
+    for s in range(len(tags)):
+        p = matrix[s][t]*tag_transitions[tags[s]][END]/tag_counts[tags[s]]
+        if p > max:
+            max = p
+            argmax = s
+
+    s = argmax
+    tagged_words = [(word, None) for word in words]
+    for t in range(len(words)-1, -1, -1):
+        word = tagged_words[t][0]
+        tagged_words[t] = (word, tags[s])
+        s = backpointer[s][t]
+    
+    return tagged_words
+
 
 
 def tag_sentence(test_file, model_file, out_file):
-    # write your code here. You can add functions as well.
+    reader = open(model_file)  
+    json_data = json.load(reader)
+    reader.close()
+
+    tag_counts = defaultdict(lambda:1, json_data['tag_counts'])
+    tag_transitions = {key:defaultdict(lambda:1,value) for (key,value) in json_data['tag_transitions'].items()}
+    start_transition = defaultdict(lambda:1, tag_transitions.pop(START))
+    word_emissions = {key:defaultdict(lambda:1,value) for (key,value) in json_data['word_emissions'].items()}
+
+    reader = open(test_file)
+    test_lines = reader.readlines()
+    reader.close()
+
+    file = open(out_file, "w")
+
+    
+    for i in range(0, len(test_lines)):
+        cur_out_line = test_lines[i].strip()
+        words = cur_out_line.split(' ')
+
+        tagged_words = viterbi(words, tag_counts, tag_transitions, start_transition, word_emissions)
+        # print(tagged_words)
+        string = ""
+        for word, tag in tagged_words:
+            string += word + "/" + tag + " "
+        string += "\n"
+        file.write(string)
+
+    file.close()
+
     print('Finished...')
 
 if __name__ == "__main__":
